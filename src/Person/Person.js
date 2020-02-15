06f386f9794c563506a71d5f962b8fa6b3c02d93
Hammam Al-Hakim <hammam_al_27rpl@student.smktelkom-mlg.sch.id>
2020-02-15 20:49:09 +0700

    Part 3.19 done

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+import "./Person.css";
+
 export default function Person(props) {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>
                 I'am {props.name} and I am {props.age}
             </p>
