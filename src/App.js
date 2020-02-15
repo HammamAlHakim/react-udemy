137cd4a83134e4ff70cbab17a2c1f9c23eb24c00
Hammam Al-Hakim <hammam_al_27rpl@student.smktelkom-mlg.sch.id>
2020-02-15 21:44:51 +0700

    Part 4.2 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-const App = props => {
-    const [personsState, setPersonsState] = useState({
+class App extends Component {
+    state = {
         persons: [
             {
                 name: "Max",
@@ -17,15 +17,13 @@ const App = props => {
                 name: "Stephanie",
                 age: 26
             }
-        ]
-    });
+        ],
+        otherState: "some other state",
+        showPerson: false
+    };
 
-    const [otherState, setOtherState] = useState("some other value");
-
-    // console.log(personsState, otherState);
-
-    const switchNameHandler = newName => {
-        setPersonsState({
+    _switchNameHandler = newName => {
+        this.setState({
             persons: [
                 {
                     name: newName,
@@ -43,8 +41,8 @@ const App = props => {
         });
     };
 
-    const nameChangeHandler = e => {
-        setPersonsState({
+    _nameChangeHandler = e => {
+        this.setState({
             persons: [
                 {
                     name: "max",
@@ -62,45 +60,54 @@ const App = props => {
         });
     };
 
-    // It must be in the render object if it class base
-    const style = {
-        backgroundColor: "white",
-        font: "inherit",
-        border: "1px solid blue",
-        padding: "8px",
-        cursor: "pointer"
+    _togglePersonHandler = () => {
+        const doesShow = this.state.showPerson;
+        this.setState({
+            showPerson: !doesShow
+        });
     };
 
-    return (
-        <div className="App">
-            <h1>Hi, I'm a React App</h1>
-            <p>This is really working!</p>
-            {/* () => inefficient way to call function handler */}
-            <button
-                onClick={() => switchNameHandler("Maximilian!!")}
-                style={style}
-            >
-                Switch Name
-            </button>
-            <Person
-                name={personsState.persons[0].name}
-                age={personsState.persons[0].age}
-            />
-            {/* .bind(this, arg) is an efficient way to call function */}
-            <Person
-                name={personsState.persons[1].name}
-                age={personsState.persons[1].age}
-                click={switchNameHandler.bind(this, "Max!")}
-                changed={nameChangeHandler}
-            >
-                My Hobbies: Racing
-            </Person>
-            <Person
-                name={personsState.persons[2].name}
-                age={personsState.persons[2].age}
-            />
-        </div>
-    );
-};
+    render() {
+        const style = {
+            backgroundColor: "white",
+            font: "inherit",
+            border: "1px solid blue",
+            padding: "8px",
+            cursor: "pointer"
+        };
+
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+                {/* () => inefficient way to call function handler */}
+                <button onClick={this._togglePersonHandler} style={style}>
+                    Toggle Persons
+                </button>
+                {this.state.showPerson ? (
+                    <div>
+                        <Person
+                            name={this.state.persons[0].name}
+                            age={this.state.persons[0].age}
+                        />
+                        {/* .bind(this, arg) is an efficient way to call function */}
+                        <Person
+                            name={this.state.persons[1].name}
+                            age={this.state.persons[1].age}
+                            click={this._switchNameHandler.bind(this, "Max!")}
+                            changed={this._nameChangeHandler}
+                        >
+                            My Hobbies: Racing
+                        </Person>
+                        <Person
+                            name={this.state.persons[2].name}
+                            age={this.state.persons[2].age}
+                        />
+                    </div>
+                ) : null}
+            </div>
+        );
+    }
+}
 
 export default App;
