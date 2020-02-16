a33050284b00ef97ab4c254a063c9e2341a033d1
Hammam Al-Hakim <hammam_al_27rpl@student.smktelkom-mlg.sch.id>
2020-02-16 21:12:24 +0700

    Part 7.7 done

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,11 @@ import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        console.log("[App.js] constructor");
+    }
+
     state = {
         persons: [
             {
@@ -26,6 +31,19 @@ class App extends Component {
         otherState: "some other state",
         showPersons: false
     };
+
+    static getDerivedStateFromProps(props, state) {
+        console.log("[App.js] getDerivedStateFromProps", props);
+        return state;
+    }
+
+    componentWillMount() {
+        console.log("[App.js] componentWillMount");
+    }
+
+    componentDidMount() {
+        console.log("[App.js] componentDidMount");
+    }
 
     _NameChangeHandler = (e, id) => {
         const personIndex = this.state.persons.findIndex(p => {
@@ -60,6 +78,7 @@ class App extends Component {
     };
 
     render() {
+        console.log("[App.js] render");
         let persons = null;
 
         if (this.state.showPersons) {
