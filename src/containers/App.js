eeb0acd448f4dd3b57249f12dec5d426ed9d8197
Hammam Al-Hakim <hammam_al_27rpl@student.smktelkom-mlg.sch.id>
2020-02-17 14:54:33 +0700

    Part 7.21 done

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 import classes from "./App.module.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
-import WithClass from "../hoc/WithClass";
+import withClass from "../hoc/withClass";
+import Auxiliary from "../hoc/Auxiliary";
 
 class App extends Component {
     constructor(props) {
@@ -103,7 +104,7 @@ class App extends Component {
         }
 
         return (
-            <WithClass classes={classes.App}>
+            <Auxiliary>
                 <button
                     onClick={() => {
                         this.setState({ showCockpit: !this.state.showCockpit });
@@ -120,12 +121,12 @@ class App extends Component {
                     />
                 ) : null}
                 {persons}
-            </WithClass>
+            </Auxiliary>
         );
     }
 }
 
-export default App;
+export default withClass(App, classes.App);
 
 // ---   IMPORTANT NOTE   ---
 // () => inefficient way to call function handler
